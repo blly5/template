7c94228f326b0bc2501f0f9b83d039aeae94db33
liyu32 <liyu32@meituan.com>
2018-09-19 16:00:07 +0800

    🐘

--- build/git.config.js
+++ build/git.config.js
@@ -3,13 +3,13 @@ const Colors = require('colors');
 
 
 let main = async ()=>{
-    console.log(`Soon`.bgCyan);
+    console.log(`Soon...`.bgBlue);
     await execa(`git`, [`add`, `.`]);
     await execa(`git`, [`commit`, `-m`, `🐘`]);
     await execa(`git`, [`push`]);
 }
 main().then(a=>{
-    console.log(`Done`.bgCyan);
+    console.log(`Done`.bgBlue);
 }).catch(a=>{
     console.log(`${a}`.bgMagenta);
 });
