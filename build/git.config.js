fc6a2eb91d36873b590419f8c1a200997eca0c79
liyu32 <liyu32@meituan.com>
2018-09-19 17:25:05 +0800

    Wed Sep 19 2018 17:25:05 GMT+0800 (CST)

--- build/git.config.js
+++ build/git.config.js
@@ -1,20 +1,18 @@
 const execa = require("execa");
 const Colors = require('colors');
 
-
+let a = new Date();
 let main = async ()=>{
     console.log(`Soon...`.bgBlue);
     await execa(`git`, [`add`, `.`]);
-    console.log(`请输入Commit ⤵️`);
-    
-    
-    let msg = await execa(`echo`,['input']);
 
-    await execa(`git`, [`commit`, `-m`, `${msg}`]); 
+    // console.log(`请输入Commit ⤵️`);
+    // let msg = await execa(`echo`,['input']);
+
+    await execa(`git`, [`commit`, `-m`, `${a}`]); 
     await execa(`git`, [`push`]);
 }
 main().then(a=>{
-    console.log(a,'123');
     console.log(`Done`.bgGreen);
 }).catch(a=>{
     console.log(`${a}`.bgMagenta);
