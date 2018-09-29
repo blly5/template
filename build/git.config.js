4cd6edbe719a1883d4c5b0b43ac3ea5a0c3a992c
liyu32 <liyu32@meituan.com>
2018-09-29 10:25:48 +0800

    1538187948623

--- build/git.config.js
+++ build/git.config.js
@@ -2,31 +2,35 @@ const execa = require("execa");
 const Colors = require('colors');
 const readline = require('readline');
 const rl = readline.createInterface(process.stdin,process.stdout);
-let msg = '';
 
-function getMessage(){
-    return  new Promise((resolve) => {
-        rl.question('commit message: ', (message) => {
-            msg = message || new Date().getTime();
-            rl.close();
-            resolve(msg)
+    let msg = '';
+
+    function getMessage(){
+        return  new Promise((resolve) => {
+            rl.question('commit message: ', (message) => {
+                msg = message || new Date().getTime();
+                rl.close();
+                resolve(msg)
+            });
         });
+    };
+
+    let main = async ()=>{
+        console.log(`Soon...`.bgBlue);
+        await execa(`git`, [`add`, `.`]);
+        await getMessage().then(a=>{
+            execa(`git`, [`commit`, `-m`, `${a}`]); 
+        });
+        await execa(`git`, [`push`]);
+    }
+    main().then(a=>{
+        console.clear();
+        console.log(`Done`.bgGreen);
+    })
+    .catch(a=>{
+        console.log(`${a}`.bgMagenta);
     });
-}
 
-let main = async ()=>{
-    console.log(`Soon...`.bgBlue);
-    await execa(`git`, [`add`, `.`]);
-    await getMessage().then(a=>{
-        execa(`git`, [`commit`, `-m`, `${a}`]); 
-    });
 
-    await execa(`git`, [`push`]);
 
-}
-main().then(a=>{
-    console.clear();
-    console.log(`Done`.bgGreen);
-}).catch(a=>{
-    console.log(`${a}`.bgMagenta);
-});
+
