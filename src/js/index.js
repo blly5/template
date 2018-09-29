3003a6f55871e94f06fa50255c2543a016e1dbf1
liyu32 <liyu32@meituan.com>
2018-09-29 10:20:48 +0800

    😆

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,2 @@
 require('../css/style.less');
 require('../js/lib/rem');
-
-var b = new Promise(function(d){
-    d();
-});
-
-
-
-let a = async function(){
-    await b();
-    console.log('123');
-};
-
-a();
